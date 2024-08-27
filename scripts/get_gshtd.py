9dd8438ba6d970530374f88931f6aeaeac4f9d30
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2024-08-27 16:33:56 +0200

    climfor 0.1.0.9005: gshtd zarr dl working

--- scripts/get_gshtd.py
+++ scripts/get_gshtd.py
@@ -8,7 +8,7 @@ nc = snakemake.output
 
 # test
 amazonia = "results/data/amazonia"
-zarr = "results/data/precipitation/chirps.zarr"
+zarr = "results/data/temperature/gshtd.zarr"
         
 # libs
 import geopandas as gp
@@ -33,43 +33,61 @@ def get_var(var, leg):
         ds = ds.transpose('time', 'lat', 'lon')
         ds = ds.rio.set_spatial_dims(x_dim="lon", y_dim="lat")
         ds = ds.rio.write_crs("epsg:4362")
-        ds = ds.where(ds.b1 > 0)
-        ds['b1'] = ds.b1*0.02 - 273.15  # K to °C
-        if(var == "tas"):
-                std_name = "temperature"
-        if(var == "tasmin"):
-                std_name = "minimum temperature"
-        if(var == "tasmax"):
-                std_name = "maximum temperature"
-        if(var == "tas"):
-                type = "mean"
-        if(var == "tasmin"):
-                type = "minimum"
-        if(var == "tasmax"):
-                type = "maximum"
-        ds.b1.attrs = {'standard_name': std_name + ' at surface', 
-                        'long_name': 'Monthly ' + type + ' daily air temperature',
-                        'units': '°C', 
-                        'explanation' : 'Monthly ' + type + ' air temperatures at 2 meters.'}
-        ds = ds.rename({'b1': var})
         return(ds)
 
-def get_gshtd(bounds):
-        leg1 = ee.Geometry.Rectangle(bounds.minx[0], bounds.miny[0], bounds.maxx[0], bounds.maxy[0])
-        ds_tas = get_var("tas", leg1)
-        ds_tasmin = get_var("tasmin", leg1)
-        ds_tasmax = get_var("tasmax", leg1)
-        ds = xr.merge([ds_tas, ds_tasmax, ds_tasmin])
-        return(ds)
-
-# code
+# each raw
 area = gp.read_file(amazonia).dissolve()
-ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com')
-gshtd = get_gshtd(area.bounds).chunk({"lon": 1000, "lat": 1000, "time": 10})
-gshtd.to_zarr(zarr)
+ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com', project="silver-link-350720")
+leg1 = ee.Geometry.Rectangle(area.bounds.minx[0], area.bounds.miny[0], area.bounds.maxx[0], area.bounds.maxy[0])
+ds_tas = get_var("tas", leg1).chunk({"lon": 1000, "lat": 1000, "time": 10})
+ds_tas.to_zarr("results/data/temperature/gshtd_tas.zarr")
+ds_tasmin = get_var("tasmin", leg1).chunk({"lon": 1000, "lat": 1000, "time": 10})
+ds_tasmin.to_zarr("results/data/temperature/gshtd_tasmin.zarr")
+ds_tasmax = get_var("tasmax", leg1).chunk({"lon": 1000, "lat": 1000, "time": 10})
+ds_tasmax.to_zarr("results/data/temperature/gshtd_tasmax.zarr")
+
+# assemble & prep each (order to be defined), save as a single zarr and remove the tmp
+# dask_jobqueue could be used to increase speed
+import xarray as xr
+ds_tas = xr.open_zarr("results/data/temperature/gshtd_tas.zarr")
+ds_tas.b1.attrs = {'standard_name': "temperature" + ' at surface', 
+                'long_name': 'Monthly ' + "mean" + ' daily air temperature',
+                'units': '°C', 
+                'explanation' : 'Monthly ' + "mean" + ' air temperatures at 2 meters.'}
+ds_tas = ds_tas.rename({'b1': "tas"})
+
+ds_tasmin = xr.open_zarr("results/data/temperature/gshtd_tasmin.zarr")
+ds_tasmin.b1.attrs = {'standard_name': " minimum temperature" + ' at surface', 
+                'long_name': 'Monthly ' + "minimum" + ' daily air temperature',
+                'units': '°C', 
+                'explanation' : 'Monthly ' + "minimum" + ' air temperatures at 2 meters.'}
+ds_tasmin = ds_tasmin.rename({'b1': "tasmin"})
+
+ds_tasmax = xr.open_zarr("results/data/temperature/gshtd_tasmax.zarr")
+ds_tasmax.b1.attrs = {'standard_name': "maximum temperature" + ' at surface', 
+                'long_name': 'Monthly ' + "maximum" + ' daily air temperature',
+                'units': '°C', 
+                'explanation' : 'Monthly ' + "maximum" + ' air temperatures at 2 meters.'}
+ds_tasmax = ds_tasmax.rename({'b1': "tasmax"})
+
+ds = xr.merge([ds_tas, ds_tasmax, ds_tasmin])
+ds = ds.chunk({"lon": 1000, "lat": 1000, "time": 10})
+ds.to_zarr("results/data/temperature/gshtd_raw.zarr")
+
+# remove null and rescale raw, save as a single zarr and remove the tmp
+# dask_jobqueue could be used to increase speed
+import xarray as xr
+ds = xr.open_zarr("results/data/temperature/gshtd_raw.zarr")
+ds = ds.where(ds.tas > 0)
+ds['tas'] = ds.tas*0.02 - 273.15  # K to °C
+ds['tasmax'] = ds.tas*0.02 - 273.15  # K to °C
+ds['tasmin'] = ds.tas*0.02 - 273.15  # K to °C
+ds.to_zarr("results/data/temperature/gshtd.zarr")
 
 # check
-# ds = xr.open_dataset(zarr)
+# import xarray as xr
 # from matplotlib import pyplot as plt
-# ds.sel(time="2001-01-01").pr.plot()
+# ds = xr.open_zarr("results/data/temperature/gshtd.zarr")
+# ds.sel(time="2001-01-01").tas.plot()
+# plt.savefig("test.png")
 # plt.show()
